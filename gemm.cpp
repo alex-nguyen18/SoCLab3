1966f48a3dddc2d24bc136b78a3c7b4e86e22b6b
Alex Nguyen <alexander.nguyen@utexas.edu>
2021-10-26 21:57:20 -0500

    latest version of gemm source so we can test independently

--- gemm.cpp
+++ gemm.cpp
@@ -1,12 +1,20 @@
 #include "gemm.h"
 
-void gemm(int M, int N, int K, INTYPE A[512*512], int lda, INTYPE B[512*512], int ldb, OUTTYPE C[512*512], int ldc)
-{    
-    for (int i = 0; i < M; ++i) {
-        for (int k = 0; k < K; ++k) {
+void gemm(const int M, const int N, const int K, const INTYPE A[512*512], const int lda, const INTYPE B[512*512], const int ldb, OUTTYPE C[512*512], const int ldc)
+{
+	#pragma HLS stream variable=A
+	#pragma HLS stream variable=B
+    loopm: for (int i = 0; i < M; ++i) {
+		//#pragma HLS unroll factor=4
+    	#pragma HLS dataflow
+        loopk: for (int k = 0; k < K; ++k) {
+			#pragma HLS unroll factor=4
             OUTTYPE A_PART = A[i * lda + k];
-            for (int j = 0; j < N; ++j) {
+            loopn: for (int j = 0; j < N; ++j) {
+				#pragma HLS pipeline
+				#pragma HLS unroll factor=8
                 C[i*ldc + j] += (A_PART*B[k*ldb + j]);// >> SHAMT;
+				#pragma HLS latency min=1 max=1
             }
         }
     }
--- gemm.h
+++ gemm.h
@@ -8,8 +8,8 @@
 //	INTYPE *A, int lda,
 //        INTYPE *B, int ldb,
 //        OUTTYPE *C, int ldc);
-void gemm(int M, int N, int K,
-INTYPE A[], int lda,
-INTYPE B[], int ldb,
-OUTTYPE C[], int ldc);
+void gemm(const int M, const int N, const int K,
+const INTYPE A[], const int lda,
+const INTYPE B[], const int ldb,
+OUTTYPE C[], const int ldc);
 
